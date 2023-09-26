2892b37d2096638c95f888cd032c3192609a65b4
Florian Bruell <florian.bruell@gmail.com>
2023-09-26 15:48:37 +0200

    update

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
+import pandas as pd
+
 print("This line will be printed.")
 print("Das ist eine neue Zeile.")
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pd.DataFrame(mydataset)
+
+print(myvar)
